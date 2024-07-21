4644d6d1f87c4a43ab56b78e03622b8468c0bd7c
Li Sevidal <106882500+Devchili@users.noreply.github.com>
2024-07-22 00:12:11 +0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,51 +1,54 @@
-$(document).ready(function(){
+$(document).ready(function() {
     //Toggle mobile navigation
-    $('.mb_menu').click(function(){
-        $('.nav_menu').toggleClass('active')
+    $('.mb_menu').click(function() {
+        $('.nav_menu').toggleClass('active');
     });
 
-    $(window).resize(function resize(){
+    // Adjust icon size based on window width
+    function adjustIconSize() {
         if ($(window).width() < 768) {
-            $('.icon_bg > i').removeClass('fa-xl');
-            $('.icon_bg > i').addClass('fa-lg');
+            $('.icon_bg > i').removeClass('fa-xl').addClass('fa-lg');
+        } else {
+            $('.icon_bg > i').removeClass('fa-lg').addClass('fa-xl');
         }
-    });
+    }
 
-    //Scroll button
-    $('#slide_right').click(function() {
+    // Initial adjustment
+    adjustIconSize();
+
+    // Adjust on window resize
+    $(window).resize(adjustIconSize);
+
+    // Scroll buttons
+    $('#slide_right').click(function(event) {
         event.preventDefault();
         $('#gallery').animate({
             scrollLeft: "+=295px"
         }, "slow");
     });
 
-    $('#slide_left').click(function() {
+    $('#slide_left').click(function(event) {
         event.preventDefault();
         $('#gallery').animate({
             scrollLeft: "-=295px"
         }, "slow");
-        
     });
 
-    //Set rooms as default display in gallery
-    $('.gallery_item').not('.rooms').hide('5000');
-    $('.gallery_item').filter('.rooms').show('5000');
+    // Set rooms as default display in gallery
+    $('.gallery_item').not('.rooms').hide(5000);
+    $('.gallery_item.rooms').show(5000);
 
-    //Filter gallery
-    $('.select_btn').click(function(){
+    // Filter gallery
+    $('.select_btn').click(function() {
         const value = $(this).attr('data-filter');
-        $('.gallery_item').not('.'+value).fadeOut().hide('1000');
-        $('.gallery_item').filter('.'+value).show('1000',function(){
-            $('#gallery').scrollLeft(0)});
-        
-    });
-        
-    //Add active class to select buttons
-    $('.select_btn').click(function(){
+        $('.gallery_item').not('.' + value).fadeOut(1000);
+        $('.gallery_item.' + value).fadeIn(1000, function() {
+            $('#gallery').scrollLeft(0);
+        });
+
+        // Add active class to select buttons
         $(this).addClass('active').siblings().removeClass('active');
         $(this).children('.bar').addClass('active_bar');
         $(this).siblings().children('.bar').removeClass('active_bar');
-    });  
+    });
 });
-
-
